d3caefe67744839872dcb47908c78a1c6e891283
Ludger Pottmeier <ludger.pottmeier@gmail.com>
2025-11-08 18:10:55 +0100

    intial version: concept works

--- internal/backend/git.go
+++ internal/backend/git.go
@@ -8,19 +8,26 @@ import (
 	"os"
 )
 
-func CloneRepo(config *config.Config, sec *config.Secret) error {
-	defer os.RemoveAll(config.RepoDir) // clean up
 
-	fmt.Println("Cloning repository...")
-	_, err := git.PlainClone(config.RepoDir, false, &git.CloneOptions{
+func CloneRepo(config *config.Config, sec *config.Secret) error {
+	os.RemoveAll(config.RepoDir) // clean up
+    os.Mkdir(config.RepoDir, 0755)
+
+	fmt.Printf("Cloning repository to...%s", config.RepoDir)
+	repo , err := git.PlainClone(config.RepoDir, false, &git.CloneOptions{
 		URL: config.RepoURL,
 		Auth: &http.BasicAuth{
 			Username: sec.GitUser, // anything except an empty string
 			Password: sec.GitToken,
 		},
 		Progress:        os.Stdout,
+        RemoteName: "origin",
 		InsecureSkipTLS: true,
+        SingleBranch: true ,
+		ReferenceName: "main",
 	})
+    repo.Head()
+
 	return err
 
 }
--- internal/config/loader.go
+++ internal/config/loader.go
@@ -1,41 +1,39 @@
 package config
 
 import (
-	
 	"os"
 
 	"gopkg.in/yaml.v2"
 )
 
 type Config struct {
-    RepoURL string `yaml:"repo_url"`
-	RepoDir string `yaml:"repo_dir"`
+	RepoURL    string `yaml:"repo_url"`
+	RepoBranch string `yaml:"repo_branch"`
+	RepoDir    string `yaml:"repo_dir"`
 }
 
 type Secret struct {
-    GitUser string `yaml:"git_user"`
-    GitToken string `yaml:"git_token"`
-	AgeKey string `yaml:"age_key"`
+	GitUser  string `yaml:"git_user"`
+	GitToken string `yaml:"git_token"`
+	AgeKey   string `yaml:"age_key"`
 }
 
 func LoadConfig(path string) (*Config, error) {
-    data, err := os.ReadFile(path)
-    if err != nil {
-        return nil, err
-    }
-    var cfg Config
-    err = yaml.Unmarshal(data, &cfg)
-    return &cfg, err
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	var cfg Config
+	err = yaml.Unmarshal(data, &cfg)
+	return &cfg, err
 }
-
 
 func LoadSecret(path string) (*Secret, error) {
-    data, err := os.ReadFile(path)
-    if err != nil {
-        return nil, err
-    }
-    var sec Secret
-    err = yaml.Unmarshal(data, &sec)
-    return &sec, err
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	var sec Secret
+	err = yaml.Unmarshal(data, &sec)
+	return &sec, err
 }
-
